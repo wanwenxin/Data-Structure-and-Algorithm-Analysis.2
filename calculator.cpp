07879c8e35754c0694ac69de95b1f7bcb439666c
wanwenxin <33198921+wanwenxin@users.noreply.github.com>
2017-11-07 09:37:14 +0800

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "Expression.h"
+#include <string>
+
+void main()
+{
+	string s;
+	cout << "Press q/Q to quit" << endl;
+	while (1)
+	{
+		cout << endl;
+		Expression expression;
+		expression.begin();
+	}
+}
